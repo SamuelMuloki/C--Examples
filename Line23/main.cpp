3cb5f95930c1fd5612ace3c552add00989fdc25d
SamuelMuloki <samuellebron0@gmail.com>
2019-05-11 15:37:36 +0300

    Initial Commit

---
+++ Line23/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Line23
+//
+//  Created by samuel muloki on 11/05/2019.
+//  Copyright © 2019 line23. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
