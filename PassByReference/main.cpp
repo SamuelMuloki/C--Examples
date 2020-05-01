4aa91e1dc3a829888e1c27ac5644ae65c33161ad
Samuel Muloki <samuellebron0@gmail.com>
2020-05-01 18:18:30 +0300

    [New] Add pass by reference

---
+++ PassByReference/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PassByReference
+//
+//  Created by Samuel Muloki on 01/05/2020.
+//  Copyright © 2020 line23. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
